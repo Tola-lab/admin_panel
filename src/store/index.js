a348530064e303bc8fcc4e8ade8d48c30201735c
Lisa <example@gmail.com>
2023-11-27 16:51:17 +0800

    add redux toolkit - configureStore

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,9 @@
-import { legacy_createStore as createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
 
-const stringMiddleware = (store) => (dispatch) => (action) => {
+const stringMiddleware = () => (dispatch) => (action) => {
     if (typeof action === 'string') {
         return dispatch ({
             type: action
@@ -12,30 +12,11 @@ const stringMiddleware = (store) => (dispatch) => (action) => {
     return dispatch(action)
 }
 
-// const enhancer = (createStore) => (...args) => {
-//     const store = createStore(...args);
-
-//     const oldDispatch = store.dispatch;
-//     store.dispatch = (action) => {
-//         if (typeof action === 'string') {
-//             return oldDispatch ({
-//                 type: action
-//             })
-//         }
-//         return oldDispatch(action)
-//     }
-// }
-
-const store = createStore(
-                combineReducers({heroes, filters}),
-                compose(
-                    applyMiddleware(stringMiddleware),
-                    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-                )
-                // compose(
-                //     enhancer,
-                //     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-                // )
-                );
+const store = configureStore({
+    reducer: {heroes, filters},
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddleware),
+    devTools: process.env.NODE_ENV !== 'production'
+    
+})
 
 export default store;
